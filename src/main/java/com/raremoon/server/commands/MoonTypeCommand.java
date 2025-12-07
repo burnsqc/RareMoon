e66bda5ab820f48422ab2cf25686c8621e1db508
burnsqc <jonathan.r.mobley@gmail.com>
2025-12-07 13:38:40 -0700

    1.19.3 error cleanup

--- src/main/java/com/raremoon/server/commands/MoonTypeCommand.java
+++ src/main/java/com/raremoon/server/commands/MoonTypeCommand.java
@@ -1,33 +1,24 @@
 package com.raremoon.server.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
-
 import com.raremoon.util.MoonType;
-
 import com.raremoon.world.level.saveddata.RareMoonOverworldExtension;
 
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
-
 
 public final class MoonTypeCommand {
 	private MoonTypeCommand() {
 	}
 
 	public static void register(CommandDispatcher<CommandSourceStack> command) {
-		command.register(Commands.literal("raremoon")
-				.requires((stack) -> stack.hasPermission(2))
-				.then(Commands.literal("normal").executes((context) -> setMoonType(context.getSource(), MoonType.NORMAL)))
-				.then(Commands.literal("blood").executes((context) -> setMoonType(context.getSource(), MoonType.BLOOD)))
-				.then(Commands.literal("fortune").executes((context) -> setMoonType(context.getSource(), MoonType.FORTUNE)))
-				.then(Commands.literal("harvest").executes((context) -> setMoonType(context.getSource(), MoonType.HARVEST)))
-				.then(Commands.literal("blue").executes((context) -> setMoonType(context.getSource(), MoonType.BLUE))));
+		command.register(Commands.literal("raremoon").requires((stack) -> stack.hasPermission(2)).then(Commands.literal("normal").executes((context) -> setMoonType(context.getSource(), MoonType.NORMAL))).then(Commands.literal("blood").executes((context) -> setMoonType(context.getSource(), MoonType.BLOOD))).then(Commands.literal("fortune").executes((context) -> setMoonType(context.getSource(), MoonType.FORTUNE))).then(Commands.literal("harvest").executes((context) -> setMoonType(context.getSource(), MoonType.HARVEST))).then(Commands.literal("blue").executes((context) -> setMoonType(context.getSource(), MoonType.BLUE))));
 	}
 
 	private static int setMoonType(CommandSourceStack stack, MoonType moonType) {
 		RareMoonOverworldExtension data = RareMoonOverworldExtension.getData(stack.getLevel().getServer());
 		data.setMoonType(moonType);
-		stack.sendSuccess(() -> moonType.getCommandResponseMessage(), true);
+		stack.sendSuccess(moonType.getCommandResponseMessage(), true);
 		return -1;
 	}
 }
