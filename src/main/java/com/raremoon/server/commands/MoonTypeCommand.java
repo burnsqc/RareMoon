7785e9a24a4122dec6beb9fcb35429ac8cf836aa
burnsqc <jonathan.r.mobley@gmail.com>
2025-12-07 14:27:11 -0700

    Update MoonTypeCommand.java

--- src/main/java/com/raremoon/server/commands/MoonTypeCommand.java
+++ src/main/java/com/raremoon/server/commands/MoonTypeCommand.java
@@ -12,7 +12,12 @@ public final class MoonTypeCommand {
 	}
 
 	public static void register(CommandDispatcher<CommandSourceStack> command) {
-		command.register(Commands.literal("raremoon").requires((stack) -> stack.hasPermission(2)).then(Commands.literal("normal").executes((context) -> setMoonType(context.getSource(), MoonType.NORMAL))).then(Commands.literal("blood").executes((context) -> setMoonType(context.getSource(), MoonType.BLOOD))).then(Commands.literal("fortune").executes((context) -> setMoonType(context.getSource(), MoonType.FORTUNE))).then(Commands.literal("harvest").executes((context) -> setMoonType(context.getSource(), MoonType.HARVEST))).then(Commands.literal("blue").executes((context) -> setMoonType(context.getSource(), MoonType.BLUE))));
+		command.register(Commands.literal("raremoon").requires((stack) -> stack.hasPermission(2))
+				.then(Commands.literal("normal").executes((context) -> setMoonType(context.getSource(), MoonType.NORMAL)))
+				.then(Commands.literal("blood").executes((context) -> setMoonType(context.getSource(), MoonType.BLOOD)))
+				.then(Commands.literal("fortune").executes((context) -> setMoonType(context.getSource(), MoonType.FORTUNE)))
+				.then(Commands.literal("harvest").executes((context) -> setMoonType(context.getSource(), MoonType.HARVEST)))
+				.then(Commands.literal("blue").executes((context) -> setMoonType(context.getSource(), MoonType.BLUE))));
 	}
 
 	private static int setMoonType(CommandSourceStack stack, MoonType moonType) {
