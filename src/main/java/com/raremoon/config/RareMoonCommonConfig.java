6163f8bc7477705be0de3ae9b6456e9f40f330ec
burnsqc <jonathan.r.mobley@gmail.com>
2025-12-08 04:50:27 -0700

    Update RareMoonCommonConfig.java

--- src/main/java/com/raremoon/config/RareMoonCommonConfig.java
+++ src/main/java/com/raremoon/config/RareMoonCommonConfig.java
@@ -41,20 +41,20 @@ public final class RareMoonCommonConfig {
 		BUILDER.push("EFFECTS");
 		BUILDER.push("BLOOD_MOON");
 		BUILDER.comment("The list of which damage sources will be multiplied during a Blood Moon can be adjusted by editing data/raremoon/tags/damage_type/blood_moon_increased.json.");
-		BLOOD_MOON_MULTIPLIER = BUILDER.comment("Damage multipliter during blood moon.").defineInRange("DAMAGE_MULTIPLIER", 2.0F, 1.0F, 100.0F);
+		BLOOD_MOON_MULTIPLIER = BUILDER.comment("Damage multipliter during a Blood Moon.").defineInRange("DAMAGE_MULTIPLIER", 2.0F, 1.0F, 100.0F);
 		BUILDER.pop();
 		BUILDER.push("FORTUNE_MOON");
 		BUILDER.comment("The list of which loot will be multiplied during a Fortune Moon can be adjusted by editing data/raremoon/tags/items/fortune_moon_increased.json.");
-		FORTUNE_MOON_MULTIPLIER = BUILDER.comment("Loot multipliter during fortune moon.").defineInRange("LOOT_MULTIPLIER", 2, 1, 100);
+		FORTUNE_MOON_MULTIPLIER = BUILDER.comment("Loot multipliter during a Fortune Moon.").defineInRange("LOOT_MULTIPLIER", 2, 1, 100);
 		BUILDER.pop();
 		BUILDER.push("HARVEST_MOON");
 		BUILDER.comment("The list of which loot will be multiplied during a Harvest Moon can be adjusted by editing data/raremoon/tags/items/harvest_moon_increased.json.");
-		HARVEST_MOON_MULTIPLIER = BUILDER.comment("Loot multipliter during harvest moon.").defineInRange("LOOT_MULTIPLIER", 2, 1, 100);
+		HARVEST_MOON_MULTIPLIER = BUILDER.comment("Loot multipliter during a Harvest Moon.").defineInRange("LOOT_MULTIPLIER", 2, 1, 100);
 		BUILDER.pop();
 		BUILDER.push("BLUE_MOON");
 		BUILDER.comment("The list of which effect may be applied during a Blue Moon can be adjusted by editing data/raremoon/tags/mob_effect/blue_moon_randomized.json.");
-		BLUE_MOON_DURATION = BUILDER.comment("Duration of random effects during blue moon in seconds.").defineInRange("EFFECT_DURATION", 15, 0, 16777215);
-		BLUE_MOON_COOLDOWN = BUILDER.comment("Time between random effects during blue moon in seconds.").defineInRange("EFFECT_COOLDOWN", 15, 0, 16777215);
+		BLUE_MOON_DURATION = BUILDER.comment("Duration of random effects during a Blue Moon in seconds.").defineInRange("EFFECT_DURATION", 15, 0, 16777215);
+		BLUE_MOON_COOLDOWN = BUILDER.comment("Time between random effects during a Blue Moon in seconds.").defineInRange("EFFECT_COOLDOWN", 15, 0, 16777215);
 		BUILDER.pop();
 		BUILDER.pop();
 
